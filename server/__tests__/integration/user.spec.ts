77b77b801081ba54a7af0cc411c21c91b5c15405
EddyPBR <edvaldojunior1310@gmail.com>
2020-12-17 14:41:24 -0300

    :heavy_check_mark: List and search user route jest test

--- server/__tests__/integration/user.spec.ts
+++ server/__tests__/integration/user.spec.ts
@@ -5,18 +5,6 @@ import app from "../../src/app";
 import User from "../../src/models/User";
 
 describe("Users route", () => {
-  // beforeAll(async () => {
-  //   if (!process.env.MONGO_URL) {
-  //     throw new Error("MongoDB server not initialized");
-  //   }
-
-  //   await mongoose.connect(process.env.MONGO_URL, {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //     useFindAndModify: true,
-  //   });
-  // });
-
   beforeAll(async () => {
     await User.deleteMany({});
   });
@@ -25,6 +13,7 @@ describe("Users route", () => {
     await mongoose.connection.close();
   });
 
+  // .CREATE
   it("should be create and save a user", async () => {
     const response = await request(app).post("/users").send({
       email: "newuser@mail.com",
@@ -43,6 +32,29 @@ describe("Users route", () => {
     expect(response.status).toBe(409);
   });
 
+  // LIST AND SEARCH
+  it("should be list all registered user", async () => {
+    const response = await request(app).get("/users");
+    expect(response.status).toBe(200);
+  });
+
+  it("should be find a user by _id param", async () => {
+    // The two first line is only to get the _id for update
+    const { _id } = await User.findOne({ email: "newuser@mail.com" }, "_id");
+    expect(_id).not.toBeFalsy();
+
+    const response = await request(app).get(`/users/${_id}`);
+    expect(response.status).toBe(200);
+  });
+
+  it("should be FAIL to find a user by _id param", async () => {
+    const _id = "5fdb68ef55619f033762babc";
+
+    const response = await request(app).get(`/users/${_id}`);
+    expect(response.status).toBe(404);
+  });
+
+  // UPDATE
   it("should be update a user succesfully", async () => {
     // The two first line is only to get the _id for update
     const { _id } = await User.findOne({ email: "newuser@mail.com" }, "_id");
@@ -57,7 +69,7 @@ describe("Users route", () => {
   });
 
   it("should be FAIL to update a user, because _id is incorectly", async () => {
-    const _id = "5fdb68ef55619f033762babc"
+    const _id = "5fdb68ef55619f033762babc";
 
     const response = await request(app).put(`/users/${_id}`).send({
       email: "updateuser@mail.com",
@@ -67,6 +79,7 @@ describe("Users route", () => {
     expect(response.status).toBe(404);
   });
 
+  // DELETE
   it("should be delete a user", async () => {
     // The two first line is only to get the _id for update
     const { _id } = await User.findOne({ email: "newuser@mail.com" }, "_id");
@@ -78,11 +91,10 @@ describe("Users route", () => {
   });
 
   it("should be FAIL to delete a user, because _id is incorectly", async () => {
-    const _id = "5fdb68ef55619f033762babc"
+    const _id = "5fdb68ef55619f033762babc";
 
     const response = await request(app).delete(`/users/${_id}`).send();
 
     expect(response.status).toBe(404);
   });
-
 });
