f5c459037e2fe4b820894be54a94afe9e3071496
EddyPBR <edvaldojunior1310@gmail.com>
2020-12-17 13:58:13 -0300

    :heavy_check_mark: Delete route jest test

--- server/__tests__/integration/user.spec.ts
+++ server/__tests__/integration/user.spec.ts
@@ -34,7 +34,7 @@ describe("Users route", () => {
     expect(response.status).toBe(201);
   });
 
-  it("should be NOT create a user, because email already exists", async () => {
+  it("should be FAIL to create a user, because email already exists", async () => {
     const response = await request(app).post("/users").send({
       email: "newuser@mail.com",
       password: "1234abcd",
@@ -49,11 +49,11 @@ describe("Users route", () => {
     expect(_id).not.toBeFalsy();
 
     const response = await request(app).put(`/users/${_id}`).send({
-      email: "updateuser@mail.com",
+      email: "newuser@mail.com",
       password: "abcd1234",
     });
 
-    expect(response.status).toBe(201);
+    expect(response.status).toBe(200);
   });
 
   it("should be FAIL to update a user, because _id is incorectly", async () => {
@@ -64,7 +64,25 @@ describe("Users route", () => {
       password: "abcd1234",
     });
 
-    expect(response.status).toBe(500);
+    expect(response.status).toBe(404);
+  });
+
+  it("should be delete a user", async () => {
+    // The two first line is only to get the _id for update
+    const { _id } = await User.findOne({ email: "newuser@mail.com" }, "_id");
+    expect(_id).not.toBeFalsy();
+
+    const response = await request(app).delete(`/users/${_id}`).send();
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should be FAIL to delete a user, because _id is incorectly", async () => {
+    const _id = "5fdb68ef55619f033762babc"
+
+    const response = await request(app).delete(`/users/${_id}`).send();
+
+    expect(response.status).toBe(404);
   });
 
 });
