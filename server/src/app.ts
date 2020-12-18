0eefda88010333611cbb22136f404cb7458e1433
EddyPBR <edvaldojunior1310@gmail.com>
2020-12-18 16:18:42 -0300

    :package: Adding middleware data verification with celebrate

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import { errors } from "celebrate";
+
 import routes from "./routes";
 
 import "./database/connection";
@@ -9,5 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 export default app;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { celebrate, Joi } from "celebrate";
 
 import checkAuthentication from "@middlewares/authentication";
 
@@ -11,12 +12,63 @@ const sessionController = new SessionController();
 const routes = express.Router();
 
 routes.get("/users", userController.index);
-routes.get("/users/:_id", userController.search);
-routes.post("/users", userController.create);
-routes.put("/users/:_id", userController.update);
-routes.delete("/users/:_id", userController.delete);
+routes.get(
+  "/users/:_id",
+  celebrate({
+    params: Joi.object().keys({
+      _id: Joi.string().required().not("null").not("undefined"),
+    }),
+  }),
+  userController.search
+);
+routes.post(
+  "/users",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    }),
+  }),
+  userController.create
+);
+routes.put(
+  "/users/:_id",
+  celebrate({
+    params: Joi.object().keys({
+      _id: Joi.string().required().not("null").not("undefined"),
+    }),
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    }),
+  }),
+  userController.update
+);
+routes.delete(
+  "/users/:_id",
+  celebrate({
+    params: Joi.object().keys({
+      _id: Joi.string().required().not("null").not("undefined"),
+    }),
+  }),
+  userController.delete
+);
 
-routes.post("/sessions", sessionController.authenticate);
+routes.post(
+  "/sessions",
+  celebrate(
+    {
+      body: Joi.object().keys({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  sessionController.authenticate
+);
 routes.get("/sessions", checkAuthentication, sessionController.check);
 
 export default routes;
