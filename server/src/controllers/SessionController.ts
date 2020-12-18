fb4baf97916de944b4550cb9a4a49438a78a9140
EddyPBR <edvaldojunior1310@gmail.com>
2020-12-18 09:47:04 -0300

    :package: Adding sessions controller and routes with jwt

---
+++ server/src/controllers/SessionController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "@models/User";
+
+class SessionController {
+  async authenticate(request: Request, response: Response) {
+    const { email, password } = request.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return response.status(404).json({
+        error: "user not found",
+      });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return response.status(401).json({
+        error: "invalid password",
+      });
+    }
+
+    const secret = process.env.JWT_SECRET as string;
+
+    const token = jwt.sign({ id: user._id }, secret, { expiresIn: "1d" });
+
+    return response.status(200).json({
+      _id: user._id,
+      email: user.email,
+      token,
+    });
+  }
+}
+
+export default SessionController;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 
 import UserController from "@controllers/UserController";
+import SessionController from "@controllers/SessionController";
 
 const userController = new UserController();
+const sessionController = new SessionController();
 
 const routes = express.Router();
 
@@ -11,5 +13,7 @@ routes.get("/users/:_id", userController.search);
 routes.post("/users", userController.create);
 routes.put("/users/:_id", userController.update);
 routes.delete("/users/:_id", userController.delete);
+
+routes.post("/sessions", sessionController.authenticate);
 
 export default routes;
