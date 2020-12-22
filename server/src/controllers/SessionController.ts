3e56b20395c2a1c90ab329015d1b6056fa459b2d
EddyPBR <edvaldojunior1310@gmail.com>
2020-12-22 15:39:42 -0300

    :package: Setting sessionController.recoverPassword

--- server/src/controllers/SessionController.ts
+++ server/src/controllers/SessionController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
+import crypto from "crypto";
 import jwt from "jsonwebtoken";
 
 import User from "@models/User";
@@ -39,6 +40,41 @@ class SessionController {
     return response.status(200).json({
       message: "user is authenticated",
     });
+  }
+
+  async recoverPassword(request: Request, response: Response) {
+    const { email } = request.body;
+
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return response.status(404).json({
+          error: "user not found",
+        });
+      }
+
+      const token = crypto.randomBytes(20).toString("hex");
+
+      const now = new Date();
+      now.setHours(now.getHours() + 1);
+
+      await User.findByIdAndUpdate(user._id, {
+        $set: {
+          passwordResetToken: token,
+          passwordResetExpires: now,
+        },
+      });
+
+      return response.status(200).json({
+        success: "token has been generated for reset password",
+      });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({
+        error: "cannot reset password, try again",
+      });
+    }
   }
 }
 
