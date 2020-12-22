c8577779f807137e17463db4fc1bb09225a8f102
EddyPBR <edvaldojunior1310@gmail.com>
2020-12-22 15:38:57 -0300

    :package: Setting fields password reset token and expire token to User model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -4,6 +4,8 @@ import bcrypt from "bcryptjs";
 interface User extends Document {
   email: string;
   password: string;
+  passwordResetToken?: string;
+  passwordResetExpires?: Date;
 }
 
 const UserSchema = new Schema({
@@ -16,6 +18,14 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true,
+  },
+  passwordResetToken: {
+    type: String,
+    select: false,
+  },
+  passwordResetExpires: {
+    type: Date,
+    select: false,
   },
 }).pre<User>("save", function (next) {
   const user = this;
